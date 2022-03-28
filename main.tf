235c12716dac2e18dd2b5d266e618d085131acfa
Majid Khatib Shahidi <38490751+majoook@users.noreply.github.com>
2022-03-28 13:45:11 +0200

    Update main.tf
    update terraform to deploy and show IP, Ireland region.

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
 provider "aws" {
-  region     = "eu-central-1"
+  region     = "eu-west-1"
 }
+
+# Defined some variable
+
+
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "webserver01"
+}
+
+
+# Code Section
+
 
 resource "aws_security_group" "allow_web_traffic" {
   name        = "allow_web_traffic"
-  description = "to allow my web to be accesible and I be able to ssh to the server"
+  description = "to allow my web to be accesible and I be able to ssh to the server "
 
   ingress {
     description      = "HTTPS"
@@ -37,27 +50,50 @@ resource "aws_security_group" "allow_web_traffic" {
   }
 
   tags = {
-    Name = "allow_web_access"
+    Name = "allow_web_traffic"
   }
 }
 
 
+
 resource "aws_instance" "webserver" {
-    ami = "ami-0f61af304b14f15fb"
+    ami = "ami-0069d66985b09d219"
     instance_type = "t2.micro"
-    key_name = "EC2-Web-Server"
+    key_name = "EC2Instance-Irelan"
     security_groups =  ["allow_web_traffic"]
 
+    /* This Section can be added if the ECR already exist
+    user_data = <<-EOF
+                #!bin/bash
+                sudo yum update
+                sudo yum install -y 
+                sudo yum install -y yum-utils
+                sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+                sudo yum install docker-ce docker-ce-cli containerd.io
+                aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 713840126499.dkr.ecr.eu-west-1.amazonaws.com
+                docker pull 713840126499.dkr.ecr.eu-central-1.amazonaws.com/docker-flask-nginx:v1.6
+                docker run -d -p 80:80 713840126499.dkr.ecr.eu-central-1.amazonaws.com/docker-flask-nginx:v1.6
+                EOF
+    */
+
     tags = {
-        Name = "WebServer01"
+        Name = var.instance_name
     }
 }
 
-resource "aws_ecr_repository" "majidshahidi" {
+
+resource "aws_ecr_repository" "docker-flask-nginx" {
   name                 = "docker-flask-nginx"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+# To print the public IP of the Instance after creation
+
+
+output "instance_ip_addr" {
+  value = aws_instance.webserver.private_ip
 }
